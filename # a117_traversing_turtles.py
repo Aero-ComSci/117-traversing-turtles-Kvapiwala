f5f9d4718eca1d470beb2710ac186ae5b8c057d4
Kush V. <148393667+Kvapiwala@users.noreply.github.com>
2024-09-24 19:59:57 -0700

    Add files via upload

--- # a117_traversing_turtles.py
+++ # a117_traversing_turtles.py
@@ -1,34 +1,41 @@
 #   a117_traversing_turtles.py
 #   Add code to make turtles move in a circle and change colors.
 import turtle as trtl
-trtl.penup()
 
 # create an empty list of turtles
-my_turtles = []
 
-# use interesting shapes and colors
-turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
-turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
+class Turtles:
+  turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
+  turtle_colors = ["red", "blue", "green", "orange", "purple", "gold"]
 
-#adds shapes to my_turtle list by iterating through the shapes list
-for s in turtle_shapes:
-  t = trtl.Turtle(shape=s)
-  my_turtles.append(t)
+  def __init__(self):
+    self.my_turtles = []
 
-#  starting positional variables
-startx = 0
-starty = 0
 
-#for each shape added to the my_turtle list it goes to the starting position and creates the following drawing
-for t in my_turtles:
-  t.goto(startx, starty)
-  t.pendown()
-  t.right(45)     
-  t.forward(50)
 
-#	updates the positional variables for new starting locations
-  startx = startx + 50
-  starty = starty + 50
+  #adds shapes to my_turtle list by iterating through the shapes list
+  def draw(self):
+    for s in self.turtle_shapes:
+      t = trtl.Turtle(shape=s)
+      self.my_turtles.append(t)
+
+    #  starting positional variables
+    startx = 0
+    starty = 0
+
+    #for each shape added to the my_turtle list it goes to the starting position and creates the following drawing
+    for t in self.my_turtles:
+      t.goto(startx, starty)
+      t.right(45)     
+      t.forward(50)
+
+    #	updates the positional variables for new starting locations
+      startx = t.xcor()
+      starty = t.ycor()
+
+
+draw = Turtles()
+draw.draw()
 
 wn = trtl.Screen()
 wn.mainloop()
